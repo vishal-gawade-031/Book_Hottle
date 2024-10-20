b41c5400b14350ba939a098402b451a7e2ea4022
vishal gawade <vishalgawade031@gmail.com>
2024-10-20 14:08:51 +0530

    ignore the nodemodules

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
-   
+const Listing= require("./models/listing.js");
+
+const MONGOURL="mongodb://127.0.0.1:27017/Wanderlust";
+
+main().then(()=>{
+    console.log("connect to DB")
+}).catch((err)=>{
+    console.log(err);
+})
+
+async function main(){
+    await mongoose.connect(MONGOURL);
+}
+
+app.get("/testListing",async (req,res)=>{
+let sampleLesting= new Listing({
+    title: "My new villa ",
+    dicription:"by the bitch",
+    price: 1200,
+    location:"calangute goa",
+    contry:"india",
+});
+await sampleLesting.save();
+res.send("succesfull save");
+});
 
 
 
+
+//just call the rout  
 app.get("/",(req,res)=>{
     res.send("working");
 })
