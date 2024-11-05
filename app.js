e44afd9ce6e07d70829e9d6b694124c2be4b30fe
vishal gawade <vishalgawade031@gmail.com>
2024-11-05 17:37:54 +0530

    define middleware function

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing= require("./models/listing.js");
 const path = require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
+const wrapAsync=require("./utils/WrapAsync.js")
+const ExpressError=require("./utils/ExpressError.js");
 //connection of database
 const MONGOURL="mongodb://127.0.0.1:27017/Wanderlust";
 
@@ -26,62 +28,68 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+
+
 // Index routfor show all the data keep it up because it will search for id 
-app.get("/listing",async (req,res)=>{
+app.get("/listing",wrapAsync (async (req,res)=>{
     const allListing=await Listing.find({});
     res.render("listing/index.ejs",{allListing}); 
-});
+}));
 
 //create rout for add listing
-app.post("/listings",async (req,res)=>{
-    //get all the elements from page
-    const newListing=new Listing(req.body.listing);
-    await newListing.save();
-    // console.log(newListing);
-    res.redirect("/listing");
-});
+app.post("/listings",
+   wrapAsync(async (req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError( 400 , "send valid request")
+    }
+     //get all the elements from page
+     const newListing=new Listing(req.body.listing);
+     await newListing.save();
+     // console.log(newListing);
+     res.redirect("/listing");
+   
+})
+);
 
 // with the link default get req will come 
-app.get("/listing/:id/edit",async(req,res)=>{
+app.get("/listing/:id/edit",wrapAsync (async(req,res)=>{
     let {id}=req.params;
-    console.log(id);
+    // console.log(id);
     const listing=await Listing.findById(id);//it return the document from DB
     // console.log(listing);
     res.render("listing/edit.ejs",{listing});
-});
+}));
 
 //update rout 
-app.put("/listings/:id",async (req,res)=>{
+app.put("/listings/:id",wrapAsync(async (req,res)=>{
     let {id}=req.params;
     //pass listing to db for update
    await Listing.findByIdAndUpdate(id,{... req.body.listing});
    res.redirect(`/listing/${id}`);// this will redirect to show rout
-});
+}));
 
 //Delete rout
 
-app.delete("/listings/:id",async (req,res)=>{
+app.delete("/listings/:id",wrapAsync(async (req,res)=>{
     let {id}=req.params;
     let deletedListing=await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listing");
-})
+}));
 
 // rout for create listing kipping upside because it is searching for listing id
-app.get("/listing/new",(req,res)=>{
+app.get("/listing/new",wrapAsync((req,res)=>{
     res.render("listing/new.ejs");
-});
+}));
 
 
 //it will find the id and return the document
-app.get("/listing/:id",async (req,res)=>{
+app.get("/listing/:id",wrapAsync(async (req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     // console.log(listing);
     res.render("listing/show.ejs",{listing});
-});
-
-
+}));
 
 
 
@@ -98,12 +106,23 @@ app.get("/listing/:id",async (req,res)=>{
 // res.send("succesfull save");
 // });
 
+// if any rout does't match 
+// Catch-all for unmatched routes
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page not Found"));
+});
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    // Destructure from the error object
+    const { statusCode = 500, message=err.message } = err;
+    res.status(statusCode).send(message);
+});
 
 
 //just call the rout  
 app.get("/",(req,res)=>{
-    res.send("working");
+    res.render("listing/home.ejs");
 })
 
 app.listen(8080 ,()=>{
